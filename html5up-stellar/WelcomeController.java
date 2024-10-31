900dbd341bdfb493f28b4dd7f56e6d9a76b671e9
Trevion Duncan <141594207+tredun01@users.noreply.github.com>
2024-10-30 19:15:45 -0500

    Add files via upload

---
+++ html5up-stellar/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Your Bank!";
+    }
+}
